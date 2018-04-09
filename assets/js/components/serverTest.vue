05ef8c4f90f24e22c52bdab6ac254770ef7073da
William Rich <will@malamiwi.uk>
2018-04-09 21:30:15 +0100

    Add a missing file

--- assets/js/components/serverTest.vue
+++ assets/js/components/serverTest.vue
@@ -1,22 +1,26 @@
 <template>
-    <th>
-          <a href='' @click="testServer">Test</a>
-    </th>
+    <a href='' @click.prevent="testServer">Test</a>
 </template>
 
 <script>
     console.log('Loading server test Vue file');
 
   export default {
-    name: 'serverTest',
     props: ['serverid'],
-    methods: {
-        testServer() {
-            var testUrl = './network/' + this.serverid + '/connectiontest';
-            console.log(testUrl);
-        },
-    },
-
+      methods: {
+          testServer() {
+              var testUrl = '/admin/network/' + this.serverid + '/test';
+              axios.get(testUrl)
+                  .then(function (response){
+                    let responseData = JSON.parse(response.data);
+                    if (responseData.connected == false){
+                        alert("Failed to connect to server");
+                    }else{
+                        alert("Connected to server");
+                    }
+                  });
+          },
+      },
   }
 </script>
 
